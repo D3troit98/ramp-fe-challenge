d9f8afaaba4ad011981d53cfa01eb59a34ddec39
Detroit98 <duruakuebuka@gmail.com>
2023-02-15 10:37:42 +0100

    Ramp solved solution

---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,31 @@
+import classNames from "classnames"
+import { useRef } from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+  const handleLabelClick = () => {
+    if (!disabled) {
+      onChange(!checked);
+    }
+  }
+  return (
+    <div className="RampInputCheckbox--container" data-testid={inputId}>
+      <label
+        className={classNames("RampInputCheckbox--label", {
+          "RampInputCheckbox--label-checked": checked,
+          "RampInputCheckbox--label-disabled": disabled,
+        })}
+        onClick={handleLabelClick}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className="RampInputCheckbox--input"
+        checked={checked}
+        disabled={disabled}
+        onChange={() => onChange(!checked)}
+      />
+    </div>
+  )
+}
